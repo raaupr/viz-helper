c431530058737d34616cc51621c2e93767ddce6c
audi <audi@sphere.me>
2020-11-30 14:18:06 +0000

    Fix number formatting bug

--- viz.py
+++ viz.py
@@ -17,7 +17,7 @@ ERROR_BAR_TYPES = ["None", "SD", "SEM"]
 ALLOWED_OUTFILE_EXT = [".jpg", ".png", ".eps"]
 
 st.title("Visualize Chromosome Distance & Splindle Length vs Time")
-st.write("v.30112020.1")
+st.write("v.30112020.2")
 
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
@@ -97,6 +97,8 @@ if (config["DISTANCE"]["plot_distance"] and df_distance is not None) or (
     if df_length is not None:
         min_default = min(df_length.index)
         max_default = max(df_length.index)
+    min_default = float(min_default)
+    max_default = float(max_default)
 
     xlim_min = config["PLOT"]["xlim_min"]
     xlim_max = config["PLOT"]["xlim_max"]
